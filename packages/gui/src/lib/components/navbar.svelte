2cfbe6e6ea98894a3905e2fe1493c1490dff98f6
K <kay-is@users.noreply.github.com>
2024-08-06 12:26:09 +0000

    chore: release GUI v0.0.11

--- packages/gui/src/lib/components/navbar.svelte
+++ packages/gui/src/lib/components/navbar.svelte
@@ -4,7 +4,7 @@
 
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <div class="tooltip tooltip-open tooltip-right" data-tip="alpha">
+    <div class="tooltip tooltip-open tooltip-right" data-tip="v0.0.11">
       <a class="btn btn-outline text-xl" href="/">Ewigkeit</a>
     </div>
   </div>
